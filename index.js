292319d2b351c7394fc6eb916731369423df96a4
extxrrrrr <ukrit200@gmail.com>
2024-04-07 18:50:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const choices = ["Rock", "Paper", "Scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay")
+const computerScoreDisplay = document.getElementById("computerScoreDisplay")
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerchoice) {
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (playerchoice === computerChoice) {
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch (playerchoice) {
+            case "Rock":
+                result = (computerChoice === "Scissors") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "Paper":
+                result = (computerChoice === "Rock") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "Scissors":
+                result = (computerChoice === "Paper") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerchoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText", "redText");
+
+    switch (result) {
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
+}
